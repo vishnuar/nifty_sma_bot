b4401428ce56c062da11d0344154af4c157b14d7
vishnuar <vishnuar2009@gmail.com>
2025-11-17 13:22:16 +0530

    fixed bugs

--- main.py
+++ main.py
@@ -10,14 +10,7 @@ SLEEP_SECONDS = int(os.getenv("SLEEP_SECONDS", "60"))
 
 STATE_FILE = "state.json"
 
-print(BOT_TOKEN)
-print(CHAT_ID)
-
 def send_telegram(msg):
-
-    print(BOT_TOKEN)
-    print(CHAT_ID)
-
     if not BOT_TOKEN or not CHAT_ID:
         print("Missing Telegram config")
         return
